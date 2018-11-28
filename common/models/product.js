ef013f1fe1f965d5e8ad0b45c590ceb33669cbc3
Sardor Isakov <sisardor@gmail.com>
2018-11-27 22:42:34 -0800

    added tag model

--- common/models/product.js
+++ common/models/product.js
@@ -1,4 +1,11 @@
 'use strict';
+const slugify = require('slugify')
+const opt = {
+  replacement: '-',    // replace spaces with replacement
+  remove: null,        // regex to remove characters
+  lower: true          // result in lower case
+}
+
 
 module.exports = function(Product) {
   // console.log('dsfdf');
@@ -12,4 +19,30 @@ module.exports = function(Product) {
   // Product.disableRemoteMethodByName('patchOrCreate', false);
   // Product.disableRemoteMethodByName('prototype.patchAttributes', false);
 
+  Product.observe('before save', function(ctx, next) {
+    if (ctx.isNewInstance) {
+      ctx.instance.url = slugify(ctx.instance.name, opt)
+    } else {
+      ctx.data.url = slugify(ctx.data.name || ctx.currentInstance.name, opt)
+    }
+
+    next();
+  });
+  Product.observe('after save', function(ctx, next) {
+    if (ctx.isNewInstance && ctx.instance.categoryId) {
+      Product.app.models.Category.findById(ctx.instance.categoryId)
+        .then(category => {
+          let tag = {tag: category.name}
+          Product.app.models.Tag.upsertWithWhere(tag, tag)
+            .then(tag => {
+              ctx.instance.tags.add(tag.id)
+              next()
+            })
+            .catch(next)
+        })
+        .catch(next)
+    } else {
+      next()
+    }
+  });
 };
