2936becc37a484c59b2d82665e0e3ca82c5728aa
Sardor Isakov <sisardor@gmail.com>
2018-12-02 14:47:34 -0800

    update

--- common/models/category.js
+++ common/models/category.js
@@ -7,18 +7,59 @@ const opt = {
 }
 const categoryUrl = 'cat'
 
+// db.categories.insert( { _id: "MongoDB", ancestors: [ "Books", "Programming", "Databases" ], parent: "Databases" } )
+// db.categories.insert( { _id: "dbm", ancestors: [ "Books", "Programming", "Databases" ], parent: "Databases" } )
+// db.categories.insert( { _id: "Databases", ancestors: [ "Books", "Programming" ], parent: "Programming" } )
+// db.categories.insert( { _id: "Languages", ancestors: [ "Books", "Programming" ], parent: "Programming" } )
+// db.categories.insert( { _id: "Programming", ancestors: [ "Books" ], parent: "Books" } )
+// db.categories.insert( { _id: "Books", ancestors: [ ], parent: null } )
 module.exports = function(Category) {
   Category.validatesUniquenessOf('name')
-
   Category.observe('before save', function(ctx, next) {
     if (ctx.isNewInstance) {
       ctx.instance.slug = slugify(ctx.instance.name, opt)
-      ctx.instance.url = '/' + categoryUrl + '/' + ctx.instance.slug
+      ctx.instance.id = ctx.instance.name
     } else {
       ctx.data.slug = slugify(ctx.data.name || ctx.currentInstance.name, opt)
-      ctx.data.url = '/' + categoryUrl + '/' + ctx.data.slug
     }
-
     next();
   });
+
+  Category.observe('before save', function(ctx, next) {
+    if (ctx.isNewInstance) {
+      if (!ctx.instance.parent_id) {
+        ctx.instance.parent_id = ''
+        ctx.instance.path = '/' + ctx.instance.slug
+        return next();
+      } else {
+        Category.findById(ctx.instance.parent_id)
+          .then(parentCategory => {
+            ctx.instance.level = parentCategory.level + 1
+            ctx.instance.children_ids = parentCategory.children_ids.concat(ctx.instance.parent_id)
+            ctx.instance.path = parentCategory.path + '/' + ctx.instance.slug
+            return next();
+          })
+          .catch(next);
+      }
+    } else {
+      return next();
+    }
+    // for (let i = 0; i < d.results.length; i++) {
+    //   console.log(d.results[i].path)
+    //   for (let j = 0; j < d.results[i].children.length; j++) {
+    //     console.log('   ' + d.results[i].children[j].path)
+    //   }
+    //   console.log();
+    // }
+  });
+  Category.observe('before save', function(ctx, next) {
+    if (ctx.isNewInstance) {
+      if (!ctx.instance.children_ids || !ctx.instance.children_ids.length) {
+        ctx.instance.level = 0
+        ctx.instance.children_ids = []
+      }
+    }
+    next();
+  });
+
 };
--- common/models/product.js
+++ common/models/product.js
@@ -20,31 +20,44 @@ module.exports = function(Product) {
   // Product.disableRemoteMethodByName('prototype.patchAttributes', false);
 
   Product.observe('before save', function(ctx, next) {
-
     if (ctx.isNewInstance) {
       ctx.instance.url = slugify(ctx.instance.name, opt)
       ctx.instance.owner_id = ctx.options.accessToken.userId
     } else {
       ctx.data.url = slugify(ctx.data.name || ctx.currentInstance.name, opt)
     }
-
     next();
   });
+  Product.observe('before save', function(ctx, next) {
+    if (ctx.isNewInstance && ctx.instance.category_id) {
+        Product.app.models.Category.findById(ctx.instance.category_id)
+          .then(category => {
+            ctx.instance.category_path = category.children_ids
+              .concat(ctx.instance.category_id)
+
+            return next()
+          })
+          .catch(next)
+    } else {
+      return next()
+    }
+  });
   // Product.observe('after save', function(ctx, next) {
-  //   if (ctx.isNewInstance && ctx.instance.categoryId) {
-  //     Product.app.models.Category.findById(ctx.instance.categoryId)
+  //   if (ctx.isNewInstance && ctx.instance.category_id) {
+  //     Product.app.models.Category.findById(ctx.instance.category_id)
   //       .then(category => {
-  //         let tag = {tag: category.name}
-  //         Product.app.models.Tag.upsertWithWhere(tag, tag)
-  //           .then(tag => {
-  //             ctx.instance.tags.add(tag.id)
-  //             next()
-  //           })
-  //           .catch(next)
+  //         // let tag = {tag: category.name}
+  //         // Product.app.models.Tag.upsertWithWhere(tag, tag)
+  //         //   .then(tag => {
+  //         //     ctx.instance.tags.add(tag.id)
+  //         //     next()
+  //         //   })
+  //         //   .catch(next)
+  //         return next()
   //       })
   //       .catch(next)
   //   } else {
-  //     next()
+  //     return next()
   //   }
   // });
 };
