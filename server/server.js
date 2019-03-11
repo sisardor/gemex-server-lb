5f1de73dff2a51cfe92d9d0dfe6f3f998375f347
Sardor Isakov <sisardor@gmail.com>
2019-03-10 21:06:16 -0700

    Update

--- server/server.js
+++ server/server.js
@@ -5,6 +5,32 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
+
+var loopbackPassport = require('loopback-component-passport');
+var PassportConfigurator = loopbackPassport.PassportConfigurator;
+var passportConfigurator = new PassportConfigurator(app);
+
+/**
+ * Flash messages for passport
+ *
+ * Setting the failureFlash option to true instructs Passport to flash an
+ * error message using the message given by the strategy's verify callback,
+ * if any. This is often the best approach, because the verify callback
+ * can make the most accurate determination of why authentication failed.
+ */
+var flash      = require('express-flash')
+// attempt to build the providers/passport config
+var config = {};
+try {
+  config = require('../providers.json');
+} catch (err) {
+  console.trace(err);
+  process.exit(1); // fatal
+}
+
+
+
+
 app.start = function() {
   // start the web server
   return app.listen(function() {
@@ -27,3 +53,20 @@ boot(app, __dirname, function(err) {
   if (require.main === module)
     app.start();
 });
+
+
+passportConfigurator.init();
+
+// We need flash messages to see passport errors
+app.use(flash());
+
+passportConfigurator.setupModels({
+  userModel: app.models.user,
+  userIdentityModel: app.models.UserIdentity,
+  userCredentialModel: app.models.UserCredential,
+});
+for (var s in config) {
+  var c = config[s];
+  c.session = c.session !== false;
+  passportConfigurator.configureProvider(s, c);
+}
