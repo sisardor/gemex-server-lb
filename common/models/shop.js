0efab024f28cf18459abbb164e5730161007f321
Sardor Isakov <sisardor@gmail.com>
2019-05-18 22:55:05 -0700

    Update

--- common/models/shop.js
+++ common/models/shop.js
@@ -1,5 +1,19 @@
 'use strict';
+const slugify = require('slugify')
+// const async = require('async')
+const opt = {
+  replacement: '-',    // replace spaces with replacement
+  remove: null,        // regex to remove characters
+  lower: true          // result in lower case
+}
 
 module.exports = function(Shop) {
 
+  Shop.observe('before save', function(ctx, next) {
+    if (ctx.isNewInstance) {
+      ctx.instance.shop_id = slugify(ctx.instance.shopName, opt)
+      // ctx.instance.id = ctx.instance.shop_id
+    }
+    next();
+  });
 };
